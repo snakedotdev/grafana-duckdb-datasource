9ff279edf0cf84b2a5b02d154df0ba5179f98a84
Cole Arendt <cole.arendt@outlook.com>
2024-09-21 07:31:43 -0400

    customize magefile and docker dependencies for duckdb compilation

--- Magefile.go
+++ Magefile.go
@@ -4,9 +4,24 @@
 package main
 
 import (
-	// mage:import
+	"fmt"
 	build "github.com/grafana/grafana-plugin-sdk-go/build"
 )
 
-// Default configures the default target.
-var Default = build.BuildAll
+func Default() {
+	fmt.Printf("HI\n")
+
+	build.SetBeforeBuildCallback(
+		build.BeforeBuildCallback(func(cfg build.Config) (build.Config, error) {
+			cfg.EnableDebug = true
+			cfg.EnableCGo = true
+
+			return cfg, nil
+		}))
+
+	b := build.Build{}
+
+	b.LinuxARM64()
+
+	//build.BuildAll()
+}
