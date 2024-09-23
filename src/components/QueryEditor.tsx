c039b2c83aff6f992f26d9545bf46d58a4a32202
Cole Arendt <cole.arendt@outlook.com>
2024-09-23 05:26:14 -0400

    set up frontend to select table and column

--- src/components/QueryEditor.tsx
+++ src/components/QueryEditor.tsx
@@ -1,45 +1,96 @@
-import React, { ChangeEvent } from 'react';
-import { InlineField, Input, Stack } from '@grafana/ui';
+import React, {ChangeEvent, useEffect, useState} from 'react';
+import {InlineField, Input, Select, Stack} from '@grafana/ui';
 import { QueryEditorProps } from '@grafana/data';
 import { DataSource } from '../datasource';
 import { MyDataSourceOptions, MyQuery } from '../types';
+import {getBackendSrv} from "@grafana/runtime";
 
 type Props = QueryEditorProps<DataSource, MyQuery, MyDataSourceOptions>;
 
-export function QueryEditor({ query, onChange, onRunQuery }: Props) {
+export function QueryEditor({ query, onChange, onRunQuery, datasource }: Props) {
+    const [tables, setTables] = useState<Array<{ label: string; value: string }>>([]);
+    const [selectedTable, setSelectedTable] = useState<string | null>(null);
+
+    const [columns, setColumns] = useState<Array<{ label: string; value: string }>>([]);
+    const [selectedColumn, setSelectedColumn] = useState<string | null>(null);
+
   const onQueryTextChange = (event: ChangeEvent<HTMLInputElement>) => {
     onChange({ ...query, queryText: event.target.value });
   };
 
-  const onConstantChange = (event: ChangeEvent<HTMLInputElement>) => {
-    onChange({ ...query, constant: parseFloat(event.target.value) });
-    // executes the query
-    onRunQuery();
-  };
+    useEffect(() => {
+        const fetchTables = async() => {
+            try {
+                const response = await getBackendSrv().get(`/api/datasources/${datasource.id}/table`);
+                const tables = response.tables || [];
+                setTables(tables);
+            } catch (error) {
+                console.error('Error fetching tables:', error)
+            }
+        }
 
-  const { queryText, constant } = query;
+        fetchTables()
+    }, [datasource.id]);
+
+    useEffect(() => {
+        const fetchColumns = async() => {
+            if (selectedTable === null) {
+                setColumns([])
+                return
+            }
+            try {
+                const response = await getBackendSrv().get(`/api/datasources/${datasource.id}/table/${selectedTable}/column`)
+                const columns = response.columns || []
+                setColumns(columns)
+            } catch (error) {
+                console.error('Error fetching columns:', error)
+            }
+        }
+        fetchColumns()
+    }, [selectedTable, datasource.id]);
+
+  const { queryText, } = query;
+
+  // setSelectedValue(table)
 
   return (
-    <Stack gap={0}>
-      <InlineField label="Constant">
-        <Input
-          id="query-editor-constant"
-          onChange={onConstantChange}
-          value={constant}
-          width={8}
-          type="number"
-          step="0.1"
-        />
-      </InlineField>
-      <InlineField label="Query Text" labelWidth={16} tooltip="Not used yet">
-        <Input
-          id="query-editor-query-text"
-          onChange={onQueryTextChange}
-          value={queryText || ''}
-          required
-          placeholder="Enter a query"
-        />
-      </InlineField>
-    </Stack>
+      <div>
+          <Stack gap={0}>
+              <InlineField label="Table">
+                  <Select
+                      id="query-editor-table"
+                      options={tables}
+                      onChange={(value) => setSelectedTable(value?.value || null)}
+                      value={selectedTable}
+                      placeholder="Choose a table"
+                      required
+                  />
+              </InlineField>
+          </Stack>
+          <Stack gap={0}>
+              <InlineField label="Column">
+                  <Select
+                      id="query-editor-column"
+                      options={columns}
+                      onChange={(value) => setSelectedColumn(value?.value || null)}
+                      value={selectedColumn}
+                      placeholder="Choose a column"
+                      required
+                  />
+              </InlineField>
+          </Stack>
+          <Stack gap={0}>
+              <InlineField label="Query Text" labelWidth={16} tooltip="Not used yet">
+                  <Input
+                      id="query-editor-query-text"
+                      onChange={onQueryTextChange}
+                      value={queryText || ''}
+                      required
+                      placeholder="Enter a query"
+                  />
+              </InlineField>
+          </Stack>
+
+      </div>
   );
 }
