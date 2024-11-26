12881acf4242d14db86894a622ba19361df0fb54
Cole Arendt <cole.arendt@outlook.com>
2024-11-26 03:41:09 -0500

    add a separate build script
    Unfortunately Dockerfile seems to do shell / variable subsitution early and so they cannot be changed... very strange... and subprocesses inherit weirdly from ARGs?... not sure

---
+++ custom-npm/publish-all.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+ARCH=$(uname -m)
+if [ "$ARCH" = "aarch64" ]; then 
+    export VERDACCIO_PORT=4874; 
+else 
+    export VERDACCIO_PORT=4873; 
+fi
+
+echo "GOT PORT ${VERDACCIO_PORT}"
+
+# set up npm
+USER=ci
+TOKEN=$(curl -v \
+       -H "Content-Type: application/json" \
+       -X PUT \
+       -d '{"name": "ci", "password": "ciuserpassword", "email": "ci@example.com"}' \
+       "http://localhost:${VERDACCIO_PORT}/-/user/org.couchdb.user:ci" | jq -r '.token')
+
+echo "registry=http://localhost:${VERDACCIO_PORT}" > ~/.npmrc
+echo "//localhost:${VERDACCIO_PORT}/:_authToken=${TOKEN}" >> ~/.npmrc
+
+# set up grafana clone
+mkdir -p grafana
+cd grafana
+
+# git setup
+git init
+git remote add origin https://github.com/grafana/grafana.git
+git fetch --depth 1 origin fbad76007d88490d04b1bcf087ffaa0cd0c04a23
+git checkout FETCH_HEAD
+
+# publish the things...
+cd packages/grafana-data && npm publish --registry http://localhost:${VERDACCIO_PORT} && cd -
+
+cd packages/grafana-ui && npm publish --registry http://localhost:${VERDACCIO_PORT} && cd -
+
+cd packages/grafana-e2e-selectors && npm publish --registry http://localhost:${VERDACCIO_PORT} && cd -
+
+cd packages/grafana-runtime && npm publish --registry http://localhost:${VERDACCIO_PORT} && cd -
+
+cd packages/grafana-schema && npm publish --registry http://localhost:${VERDACCIO_PORT} && cd -
+
+cd packages/grafana-sql && sed -i'' 's/"private".*,//g' package.json && npm publish --registry http://localhost:${VERDACCIO_PORT} && cd -
+    
