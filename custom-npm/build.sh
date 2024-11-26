44cd131c1036b4bc83bb25909abd429629f62662
Cole Arendt <cole.arendt@outlook.com>
2024-11-26 02:45:18 -0500

    try unix sockets

--- custom-npm/build.sh
+++ custom-npm/build.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-docker build --platform=linux/amd64 -t ghcr.io/snakedotdev/grafana-duckdb-datasource-npm:0.1.0 .
+# Default repository name if not provided
+REPO=${1:-"snakedotdev"}
+VERSION="0.1.0"
+ACTION=${2:-"push"}  # Default to push if not specified
+
+# Enable Docker BuildKit for better multi-arch support
+export DOCKER_BUILDKIT=1
+
+# Create and use a new builder instance
+docker buildx create --use
+
+# Build and optionally push multi-arch images
+if [ "$ACTION" = "push" ]; then
+  PUSH_FLAG="--push"
+else
+  PUSH_FLAG="--load"
+fi
+
+docker buildx build \
+  --platform linux/amd64,linux/arm64/v8 \
+  -t ghcr.io/${REPO}/grafana-duckdb-datasource-npm:${VERSION} \
+  ${PUSH_FLAG} \
+  .
